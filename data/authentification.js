15a93cac441817fa2bd41ebe27d9c17d52906a2a
romain biremon <romainbiremon@gmail.com>
2014-01-28 21:24:37 +0100

    Auth panel informs on failure + added bootstrap css & fonts (only, no js)

--- data/authentification.js
+++ data/authentification.js
@@ -1,4 +1,13 @@
 $(function(){
+	//Cannot use jQuery's "submit()" as if I wrap in a <form> element, firefox tries to save the password and throws error because it is not a browser window...
+	
+	$("#pass").keyup(function(event){ 
+	//if user presses enter while in the password field, "click" on the submit button
+    if(event.keyCode == 13){
+        $("#submit").click();
+    }
+	});
+
 	$("#submit").click(function(){
 		var password = "";
 		if($("#pass").val()) password=$("#pass").val();
@@ -9,4 +18,8 @@ $(function(){
 self.port.on("show", function onShow(){
 	$("#pass").focus();
 	$("#pass").val("");
+	});
+	
+self.port.on("auth_fail", function (){
+	$("#input").after("<br/><div class=\"alert alert-danger\"><small>Sorry, the password is wrong</small></div>");
 	});
