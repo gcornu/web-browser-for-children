38ec8d8399bde96feb034a17bdacfd599c3ba332
Gauthier Cornu <cornu.gauthier@gmail.com>
2014-02-13 21:35:05 +0100

    Added test for adding an element to the default blacklist

--- test/test-blacklisting.js
+++ test/test-blacklisting.js
@@ -3,11 +3,16 @@ let { before, after } = require('sdk/test/utils');
 var blacklisting = require('./blacklisting');
 var timers = require('sdk/timers');
 
-exports['test remove default blacklist'] = function (assert) {
+exports['test default blacklist'] = function (assert) {
 	blacklisting.removeElementsFromDefaultBlacklist(['*.mozilla.org'], 'General');
 	var defaultBlacklist = blacklisting.getDefaultBlacklist();
 	var index = defaultBlacklist['General'].indexOf('*.mozilla.org');
 	assert.ok(index === -1, 'Remove element from default blacklist ok');
+
+	blacklisting.addElementsToDefaultBlacklist(['*.mozilla.org'], 'General');
+	var defaultBlacklist = blacklisting.getDefaultBlacklist();
+	var index = defaultBlacklist['General'].indexOf('*.mozilla.org');
+	assert.ok(index !== -1, 'Add element to default blacklist ok');
 };
 
 before(exports, function (name, assert, done) {
