11fd15e61a806ea1757d48ee9b04c74f65463448
Gauthier Cornu <cornu.gauthier@gmail.com>
2014-02-14 10:53:41 +0100

    Updated blacklist tests

--- test/test-blacklisting.js
+++ test/test-blacklisting.js
@@ -17,6 +17,22 @@ exports['test default blacklist'] = function (assert) {
 	assert.ok(index !== -1, 'Add element to default blacklist');
 };
 
+exports['test default blacklist errors'] = function (assert) {
+	assert.throws(function () {
+			blacklisting.addElementsToDefaultBlacklist(['*.mozilla.org'], 'General');
+		},
+		/Host is not removed from default blacklist/,
+		'Add unremoved element to default blacklist error'
+	);
+	blacklisting.removeElementsFromDefaultBlacklist(['*.mozilla.org'], 'General');
+	assert.throws(function () {
+			blacklisting.removeElementsFromDefaultBlacklist(['*.mozilla.org'], 'General');
+		},
+		/Host is already removed from default blacklist/,
+		'Remove already removed element from default blacklist error'
+	);
+}
+
 exports['test custom blacklist'] = function (assert, done) {
 	var index;
 	blacklisting.addElementToCustomBlacklist('www.test.com', 'Default');
@@ -39,6 +55,32 @@ exports['test custom blacklist'] = function (assert, done) {
 	});
 }
 
+exports['test custom blacklist errors'] = function (assert) {
+	blacklisting.addElementToCustomBlacklist('www.test.com', 'Default');
+	assert.throws(function () {
+			blacklisting.removeElementsFromCustomBlacklist(['*.anothertest.com'], 'Default');
+		},
+		/Host is not in the custom blacklist/,
+		'Remove element not in the custom blacklist error'
+	);
+	blacklisting.removeElementsFromCustomBlacklist(['*.test.com'], 'Default');
+	console.log(ss.storage.customBlacklist);
+	assert.throws(function () {
+			blacklisting.removeElementsFromCustomBlacklist(['*.anothertest.com'], 'Default');
+		},
+		/The given category contains nothing/,
+		'Remove element from empty category of custom blacklist error'
+	);
+	blacklisting.addElementToCustomBlacklist('www.test.com', 'Default');
+	console.log(ss.storage.customBlacklist);
+	assert.throws(function () {
+			blacklisting.addElementToCustomBlacklist('www.test.com', 'Default');
+		},
+		/Host is already in custom blacklist/,
+		'Add already added element to the custom blacklist error'
+	);
+}
+
 before(exports, function (name, assert, done) {
 	blacklisting.setInitCallback(function () {
     	done();
@@ -47,6 +89,11 @@ before(exports, function (name, assert, done) {
 	portStub.emit = function () {};
 	blacklisting.setPort(portStub);
   	blacklisting.init(true);
+});
+
+after(exports, function (name, assert, done) {
+	delete ss.storage.customBlacklist;
+	done();
 });
 
 require('sdk/test').run(exports);
