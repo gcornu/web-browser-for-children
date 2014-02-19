01698932a8ae30cb5f07f7429a8eeea1bec23605
Gauthier Cornu <cornu.gauthier@gmail.com>
2014-02-19 13:55:36 +0100

    Activate/Deactivate option unified

--- data/panels/menu.js
+++ data/panels/menu.js
@@ -3,4 +3,10 @@ $(function () {
 		var id = $(this).attr('id');
 		self.port.emit(id);
 	});
+
+	$('#activate, #deactivate').click(function () {
+		var label = $(this).attr('id') == 'activate' ? 'Deactivate' : 'Activate';
+		$(this).attr('id', label.toLowerCase());
+		$(this).html(label + 'FfC');
+	});
 });
