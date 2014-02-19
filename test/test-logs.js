f8092ac0c6c70c4fe5e81ab6a52f05fdb2dbc88e
romain biremon <romainbiremon@gmail.com>
2014-02-19 15:07:03 +0100

    Added log test

--- test/test-logs.js
+++ test/test-logs.js
@@ -1,4 +1,6 @@
 var logs = require("./logs");
+var _ = require("./underscore");
+
 
 exports["test auhentication log"] = function(assert, done) {
 	var portStub = new Object();
@@ -13,6 +15,53 @@ exports["test auhentication log"] = function(assert, done) {
 	logs.addToLoginLog(true);
 	logs.addToLoginLog(false);
 	logs.getLoginLog();
+};
+
+
+exports["test history log"] = function(assert,done) { //for now, requires tester to close tab to validate. Change behavior?
+	// turn on history logging
+	logs.startHistoryLogging();
+	
+	//navigate to a website
+	var tabs = require('sdk/tabs');
+	var pageToVisit="http://example.com";
+	tabs.open({
+		url: pageToVisit,
+		onClose: callback
+	});
+	function callback() {
+		require("sdk/timers").setTimeout(verifyLog(pageToVisit),5000);
+	}
+	function verifyLog(url) {
+		//strip url of http prefix, www and trailing slash, as browser may or may not automatically add them
+		function removeUrlPrefix(url) {
+			prefix = /(^https?:\/\/|www\.|\/$)/g;
+			// remove any prefix
+			url = url.replace(prefix, "");
+			//url = url.replace(/www\/./, "");
+			return url;
+		}
+	//check that the website has been logged
+		var fileHandler = require('./fileHandler');
+		//fileHandler.writeJSON({history:[url]},'history.json');
+		fileHandler.readJSON('history.json', function(data) {
+			console.log("history url: "+(_.last(data.history)).url);
+			assert.ok(_.isEqual(removeUrlPrefix((_.last(data.history)).url),removeUrlPrefix(url)),"log visited website success");
+			done();
+		});
+	}
+	
+
+	
+	//turn off history logging
+	logs.stopHistoryLogging();
+	
+	//navigate to a website
+	
+	//chack that website has not been logged
+	
+	//clear history log
+	
 };
 
 require("sdk/test").run(exports);
