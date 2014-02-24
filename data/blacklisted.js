705d7db68a6fa741490a871047bc3abbe199860b
Gauthier Cornu <cornu.gauthier@gmail.com>
2014-02-24 12:31:53 +0100

    Using readFile instead of streamFile: increase performances

--- data/blacklisted.js
+++ data/blacklisted.js
@@ -1,13 +1,22 @@
 // stop loading of the page
-window.stop();
+//window.stop();
+$('html').hide();
 
-// replace page content and title
-document.documentElement.innerHTML = '<h1>This page is blacklisted. Go away!</h1>';
-document.title = 'Forbidden website';
+self.port.on('allow', function () {
+	$('html').show();
+});
 
-// set favicon to null
-var link = document.createElement('link');
-link.type = 'image/x-icon';
-link.rel = 'shortcut icon';
-link.href = '';
-document.getElementsByTagName('head')[0].appendChild(link);
+self.port.on('deny', function () {
+	window.stop();
+	// replace page content and title
+	document.documentElement.innerHTML = '<h1>This page is blacklisted. Go away!</h1>';
+	document.title = 'Forbidden website';
+	$('html').show();
+
+	// set favicon to null
+	var link = document.createElement('link');
+	link.type = 'image/x-icon';
+	link.rel = 'shortcut icon';
+	link.href = '';
+	document.getElementsByTagName('head')[0].appendChild(link);
+});
