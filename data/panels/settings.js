4bcf18dd37b3825d19180c9030ce8c2f5506dbf8
Gauthier Cornu <cornu.gauthier@gmail.com>
2014-02-27 11:57:26 +0100

    Using a tab for settings instead of a panel

--- data/panels/settings.js
+++ data/panels/settings.js
@@ -11,39 +11,39 @@ $(function () {
 	/**
 	* Password page submit action
 	*/
-	$("input.password").keyup(function(event){ 
+	$("input.password").keyup(function (event) { 
 		//if user presses enter while in a password field, "click" on the submit button
-		if(event.keyCode == 13){
+		if(event.keyCode == 13) {
 			$("#change_pass").click();
 		}
 	});
 	
-	$("#change_pass").click(function(){
+	$("#change_pass").click(function () {
 		$(".alert").hide(); //remove any leftover alert
-		var problem = (function(){ //do some validation and generate a message if necessary
-			if($("#new_pass1").val()!==$("#new_pass2").val()){
+		var problem = (function () { //do some validation and generate a message if necessary
+			if($("#new_pass1").val() !== $("#new_pass2").val()) {
 				return "Passwords must match";
-			} else if($("#new_pass1").val().length<4){
+			} else if($("#new_pass1").val().length<4) {
 				return "New password must be more than 4 characters long";
+			} else {
+				return "";
 			}
-			else { return "";}
 		})();
-		if(problem===""){ //if there was no validation error, send old and new passwords
+		if(problem === ""){ //if there was no validation error, send old and new passwords
 			var pwords = {};
-			pwords.oldpass=$("#old_pass").val();
-			pwords.newpass=$("#new_pass1").val();
-			self.port.emit("update_pass",pwords);
-			}
-		else inform(problem,"error");
+			pwords.oldpass = $("#old_pass").val();
+			pwords.newpass = $("#new_pass1").val();
+			self.port.emit("update_pass", pwords);
+		} else inform(problem,"error");
 			//in case there was a message generated, append it to the page.
 	});
 
     /**
      *
-    */
+     */
     $("input:radio[name=filteringOptions]").click(function () {
-        var val=$(this).val();
-        self.port.emit("filter",val); //val can be none, wlist or blist
+        var val = $(this).val();
+        self.port.emit("filter", val); //val can be none, wlist or blist
     });
 	
 	/**
@@ -79,13 +79,13 @@ $(function () {
 							'#remove-custom-blacklist, ' +
 							'#remove-custom-whitelist';
 
-	$(idHandledButtons).click(function() {
+	$(idHandledButtons).click(function () {
 		var self = this;
 		var paramArray = $(self).attr('id').split('-');
 		listsButtonHandler.apply(null, paramArray);
 	});
 
-	$('#add-custom-blacklist, #add-custom-whitelist').click(function() {
+	$('#add-custom-blacklist, #add-custom-whitelist').click(function () {
 		var listName = $(this).attr('id').split('-').pop();
 		var uri = window.prompt('Please enter the URL you want to add to the ' + listName + ':');
 		if(uri) {
@@ -94,7 +94,7 @@ $(function () {
 		}
 	});
 
-	$('#add-custom-blacklist-category, #add-custom-whitelist-category').click(function() {
+	$('#add-custom-blacklist-category, #add-custom-whitelist-category').click(function () {
 		var category = window.prompt('Name of the new category:');
 		if(category) {
 			var id = $(this).attr('id').split('-');
@@ -197,7 +197,7 @@ self.port.on("change_pass_result",function(result){
 	if(result) {
 		inform("Password successfully changed","success"); 
         $("#nav").hide();
-        $(".container").hide();
+        $(".tab_container").hide();
         setTimeout(function(){
 			self.port.emit("password_done");
 			$("#nav").show();
@@ -210,13 +210,13 @@ self.port.on("change_pass_result",function(result){
 	else inform("Password was not changed. Is your old password correct?","error");
 });
 
-self.port.on("set_first_password", function(){
+self.port.on("set_first_password", function () {
 	showTab("pass");
 	$("#old_pass").parent().hide();
 	$(".panel").prepend("<div id=\"welcome\"><h2>Welcome to the Firefox for children extension</h2><p>Please set your parent password below:</p></div>");
 });
 
-self.port.on("current_filter", function(value){
+self.port.on("current_filter", function (value) {
 	$('#gen_tab input[name="filteringOptions"][value="' + value + '"]').prop('checked', true);
 });
 
@@ -321,9 +321,10 @@ function fillListsDivsHelper(list, prefix) {
 	var title = $('#' + prefix + '-inner h3');
 	$('#' + prefix + '-inner').empty().append(title);
 
-	Object.keys(list).forEach(function(category) {
+	Object.keys(list).forEach(function (category) {
 		var div = $('<div>').attr('id', prefix + '-category-' + category);
-		list[category].forEach(function(elem) {
+		list[category].forEach(function (elem) {
+			alert(elem);
 			div.append('<input type="checkbox" id="' + elem + '"/><label for="' + elem + '">' + elem + '</label><br/>');
 		});
 		$('#' + prefix + '-inner').append(div);
@@ -341,17 +342,17 @@ function fillListsDivsHelper(list, prefix) {
  */
 function fillMenu(list, prefix, removedPrefix) {
 	if($('#' + prefix + '-categories select').is(':empty')) {
-		Object.keys(list).forEach(function(category) {
+		Object.keys(list).forEach(function (category) {
 			var option = $('<option>').attr('value', category).html(category.replace('_', ' '));
 			$('#' + prefix + '-categories select').append(option);
 		});
 		$('#' + prefix + '-categories select').first('option').prop('selected', 'selected');
-		$('#' + prefix + '-categories select').change(function() {
+		$('#' + prefix + '-categories select').change(function () {
 			$('#' + prefix + '-inner div').hide();
 			$('#' + prefix + '-category-' + $(this).val()).show();
 		});
 		if(removedPrefix) {
-			$('#' + prefix + '-categories select').change(function() {
+			$('#' + prefix + '-categories select').change(function () {
 				$('#' + removedPrefix + '-inner div').hide();
 				$('#' + removedPrefix + '-category-' + $(this).val()).show();
 			});
@@ -469,7 +470,7 @@ function fillHistoryReport(visits) {
 
 function showTab(tab_choice) { //hides other content and shows chosen tab "pass","gen","lists" or "report"
 	self.port.emit("tab_choice",tab_choice);
-	$(".container").hide();
+	$(".tab_container").hide();
 	$(".alert").hide(); //remove leftover alerts
 	$("#"+tab_choice+"_tab").show();
 	$("#nav .active").removeClass("active");
