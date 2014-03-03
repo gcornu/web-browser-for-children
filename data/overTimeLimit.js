cbaa46e71c17d750742d88535ab1f2874676830a
Gauthier Cornu <cornu.gauthier@gmail.com>
2014-03-03 22:44:15 +0100

    Time limit for each category can be chosen in the settings tab. Display message when time limit is exceeded

---
+++ data/overTimeLimit.js
@@ -0,0 +1,20 @@
+// stop loading of the page
+window.stop();
+
+// replace page content and title
+
+document.documentElement.innerHTML = '<div class="container" style="margin-top:20%"><div class="jumbotron"><h1>&#9785; Sorry, you exceeded your time limit for this category of websites!</h1></div></div>';
+document.title = 'Forbidden website';
+
+// set favicon to null
+var link = document.createElement('link');
+link.type = 'image/x-icon';
+link.rel = 'shortcut icon';
+link.href = '';
+document.getElementsByTagName('head')[0].appendChild(link);
+
+var css = document.createElement('link');
+css.type = "text/css";
+css.rel = "stylesheet";
+css.href = "//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css";
+document.getElementsByTagName('head')[0].appendChild(css);
--- data/panels/settings.js
+++ data/panels/settings.js
@@ -75,6 +75,11 @@ $(function () {
 		$('#login').click();
 	});
 
+	$('#limit_time').click(function (e) {
+		e.preventDefault();
+		self.port.emit('limit_time_tab_clicked');
+	})
+
 	var idHandledButtons =	'#remove-default-blacklist, ' +
 							'#add-default-blacklist, ' +
 							'#remove-default-whitelist, ' +
@@ -260,6 +265,10 @@ self.port.on('history_log_read', function (visits) {
 self.port.on('show_gen', function () {
 	$('#gen').click();
 });
+
+self.port.on('time_limit_initialized', function (categories) {
+	fillTimeLimitSelect(categories);
+})
 
 /**
  * Event handler when the 'add' button is clicked for custom lists
@@ -468,7 +477,26 @@ function fillHistoryReport(visits) {
 	});
 	$("#table-history").trigger("update"); //trigger update so that tablesorter reloads the table
 	$(".tablesorter-filter").addClass("form-control input-md");
+}
 
+/*
+ * Fill the select for the time limitation
+ *
+ * @param {array} categories to be added to the select element
+ */
+function fillTimeLimitSelect (categories) {
+	if(categories.length === 0) {
+		$('#limitTimeOptions').html('No category defined');
+	} else {
+		categories.forEach(function (category) {
+			var option = $('<option>').attr('value', category).html(category.replace('_', ' '));
+			$('#limit_time_tab select').append(option);
+		});
+		$('input:radio[name=limitTimeOptions]').click(function () {
+			var category = $('#limit_time_tab select option:selected').val();
+			self.port.emit('limit_time_choice', category, $(this).val());
+		});
+	}
 }
 
 function showTab(tab_choice) { //hides other content and shows chosen tab "pass","gen","lists" or "report"
