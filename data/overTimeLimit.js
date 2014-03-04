e8c7387ea846e46999fa17ac66e0564c3944a2ad
Gauthier Cornu <cornu.gauthier@gmail.com>
2014-03-04 22:37:11 +0100

    USing local bootstrap style in overtime panel

--- data/overTimeLimit.js
+++ data/overTimeLimit.js
@@ -2,7 +2,6 @@
 window.stop();
 
 // replace page content and title
-
 document.documentElement.innerHTML = '<div class="container" style="margin-top:20%"><div class="jumbotron"><h1>&#9785; Sorry, you exceeded your time limit for this category of websites!</h1></div></div>';
 document.title = 'Time limit exceeded';
 
@@ -12,9 +11,3 @@ link.type = 'image/x-icon';
 link.rel = 'shortcut icon';
 link.href = '';
 document.getElementsByTagName('head')[0].appendChild(link);
-
-var css = document.createElement('link');
-css.type = "text/css";
-css.rel = "stylesheet";
-css.href = "//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css";
-document.getElementsByTagName('head')[0].appendChild(css);
