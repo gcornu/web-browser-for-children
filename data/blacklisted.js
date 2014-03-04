66255968d38d5caecd671ac141768290a51ae84f
Gauthier Cornu <cornu.gauthier@gmail.com>
2014-03-04 22:30:12 +0100

    USing local bootstrap style in blacklisted panel

--- data/blacklisted.js
+++ data/blacklisted.js
@@ -2,8 +2,6 @@
 window.stop();
 
 // replace page content and title
-console.log(css);
-
 document.documentElement.innerHTML = '<div class="container" style="margin-top:20%"><div class="jumbotron"><h1>&#9785; Sorry, you are not allowed here!</h1></div></div>';
 document.title = 'Forbidden website';
 
@@ -13,9 +11,3 @@ link.type = 'image/x-icon';
 link.rel = 'shortcut icon';
 link.href = '';
 document.getElementsByTagName('head')[0].appendChild(link);
-
-var css = document.createElement('link');
-css.type = "text/css";
-css.rel = "stylesheet";
-css.href = "//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css";
-document.getElementsByTagName('head')[0].appendChild(css);
