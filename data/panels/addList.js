2687c6ff2bdb1edb6502b2014009d508ca1ed43e
Gauthier Cornu <cornu.gauthier@gmail.com>
2014-03-13 12:27:24 +0100

    Beginning style of add to lists panels

---
+++ data/panels/addList.js
@@ -0,0 +1,64 @@
+$(function() {
+	$("#add").html('Add to blacklist');
+
+	$('#new_category').click(function () {
+		var category = window.prompt('Name of the new category:');
+		if(category) {
+			var select = $('select');
+			if(select.find('option[value="' + category.replace(' ', '_') + '"]').length === 0) {
+				select.append(createOption(category).prop('selected', 'selected'));
+				select.prop('disabled', false);
+				select.selectpicker('refresh');
+			} else {
+				inform('This category already exists', 'danger', 5000);
+			}
+		}
+	});
+
+	$("#submit").click(function () {
+		var category = $('select option:selected').val();
+		if(!category) {
+			alert('Please select a category.');
+		} else {
+			self.port.emit('add_blacklist', category);
+		}
+	});
+});
+
+self.port.on('categories', function (categories) {
+	var select = $('select');
+
+	select.empty();
+	if(categories.length > 0) {
+		categories.forEach(function (category) {
+			select.append(createOption(category));
+		});
+	} else {
+		select.prop('disabled', true);
+	}
+
+	select.selectpicker('refresh');
+	select.selectpicker('setStyle');
+});
+
+/**
+ * This function creates option element for selects in lists
+ *
+ * @param {string} label of the option
+ */
+function createOption(label) {
+	return $('<option>', {'value': label.replace(' ', '_'), 'text': label.replace('_', ' ')});
+}
+
+function inform(message, alertClass, timeout) {
+	$('#message_container').append($('<div>', {'id': 'inform', 'style': 'height: 30px; padding-top: 5px; padding-bottom: 5px;', 'class': 'alert alert-' + alertClass})
+									.append($('<small>', {'text': message})));
+
+	if(timeout) {
+		setTimeout(function () {
+			$('#message_container #inform').fadeOut(500, function () {
+				$('#message_container #inform').remove();
+			});
+		}, timeout);
+	}
+}
