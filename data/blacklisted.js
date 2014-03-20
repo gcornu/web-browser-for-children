44c175a3cf044d68e39ede8811f85244202d4de4
Gauthier Cornu <cornu.gauthier@gmail.com>
2014-03-20 18:47:56 +0100

    Fixed a bug when blacklist server was responding too fast

--- data/blacklisted.js
+++ data/blacklisted.js
@@ -8,6 +8,9 @@ self.port.on('allow', function () {
 self.port.on('deny', function () {
 	window.stop();
 	// replace page content and title
+	if($('body').length === 0) {
+		$('html').append($('<body>'));
+	}
 	$('body').empty();
 	$('body').append($('<div>', {'class': 'container', 'style': 'margin-top:20%'})
 				.append($('<div>', {'class': 'jumbotron'})
