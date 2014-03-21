6462222b52ba083bfa15c8a279e7a47bb00012ed
florian.clerc@ecl2014.ec-lyon.fr <florian.clerc@ecl2014.ec-lyon.fr>
2014-03-21 23:01:29 +0100

    Send favorites to the toolbar

--- data/fav_toolbar.js
+++ data/fav_toolbar.js
@@ -1,1 +1,19 @@
-window.alert("Page matches ruleset");
+//window.alert("Page matches ruleset");
+$('body').prepend('hello');
+self.port.emit('require_favorites_toolbar');
+
+self.port.on('favorites_toolbar' , function(){
+console.log('hello2');
+	//window.alert("Page matches ruleset");
+	
+	
+	var style = "padding : 0px; margin : 0px; margin-top:2px; background-color:blue; width:100%; height:100px;";
+
+	var fav_toolbar=$('<div>');
+	$(fav_toolbar).addClass('fav_toolbar');
+	$(fav_toolbar).attr('style', style);
+	$(fav_toolbar).prepend('Hello');
+
+
+	$('body').prepend(fav_toolbar);
+});
