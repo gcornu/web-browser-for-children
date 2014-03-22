1d134dee1bbc4d4e0c274b19dacc24f8b5b90109
florian.clerc@ecl2014.ec-lyon.fr <florian.clerc@ecl2014.ec-lyon.fr>
2014-03-22 12:03:34 +0100

    Setting mechanism structure to add favorites

--- data/fav_toolbar.js
+++ data/fav_toolbar.js
@@ -1,10 +1,16 @@
+//The fav_toolbar which is going to be displayed at the top of all pages
+var fav_toolbar=$('<div>');
+$(fav_toolbar).addClass('child_fav_toolbar');
+	
+//The var which is going to contain the star-icon and event to add a favorite.
+var add_favorite_image=$('<img>');
+	
+
 self.port.on('favorites_toolbar' , function(data_received){
 	
 	var favorites=data_received.favorites
-	
-	var fav_toolbar=$('<div>');
-	$(fav_toolbar).addClass('child_fav_toolbar');
-	
+		
+	//Adding the favorites to the toolbar with favicon images and url
 	$(favorites).each(function(index, favorite){
 		var link=$('<a>').attr('href' , favorite.uri);
 		var image = $('<img>');
@@ -13,11 +19,27 @@ self.port.on('favorites_toolbar' , function(data_received){
 		$(fav_toolbar).append(link);
 	});
 	
-	var add_favorite_image=$('<img>');
+	
 	$(add_favorite_image).attr('src' , data_received.star).addClass('favorite_button');
 	$(fav_toolbar).append(add_favorite_image);
-
+	$(add_favorite_image).click(function(){
+		self.port.emit('add_favorite' , document.URL);
+	
+	
+	});
+	
+	
 	$('body').prepend(fav_toolbar);
+	
+	
+	
+	self.port.on('add_favorite_answer' , function(answer_image){
+		var answer_image_container=$('<img>');
+		$(answer_image_container).attr('src' , answer_image).addClass('answer_image');
+		$(fav_toolbar).append(answer_image_container);
+	
+	
+	});
 });
 
 
