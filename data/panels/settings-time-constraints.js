0ae292ca0fd221e7a3b3fc367aee61397407d651
Gauthier Cornu <cornu.gauthier@gmail.com>
2014-03-22 14:31:41 +0100

    Made a tbale for hour selection in settings.js

--- data/panels/settings-time-constraints.js
+++ data/panels/settings-time-constraints.js
@@ -17,6 +17,47 @@ $(function () {
 		self.port.emit('hour_constraints_type_set', val); //val can be overall or categories
 		handleHourConstraintsOptionClick(val);
 	});
+
+	/**
+	 * Init sliders
+	 */
+	$('#hour_constraints-pane .slider-morning').slider({
+		range: true,
+		min: 0,
+		max: 840,
+		step: 15,
+		values: [480, 720],
+		slide: updateTooltip
+	});
+
+	$('#hour_constraints-pane .slider-afternoon').slider({
+		range: true,
+		min: 720,
+		max: 1440,
+		step: 15,
+		values: [840, 1140],
+		slide: updateTooltip
+	});
+
+	$('#hour_constraints').click(function () {
+		// init sliders tooltips
+		var sliders = $('#hour_constraints-pane .slider');
+		sliders.each(function (index, slider) {
+			var slider = $(this);
+			var handles = slider.find('.ui-slider-handle');
+			slider.slider('option', 'slide')(null, { value: slider.slider('values', 0), handle: handles[0] });
+			slider.slider('option', 'slide')(null, { value: slider.slider('values', 1), handle: handles[1] });
+		});
+	});
+
+	$('#hour-constraints-table input:checkbox').change(function () {
+		var slider = $('#' + $(this).attr('id').replace('checkbox', 'slider'));
+		if($(this).is(':checked')) {
+			slider.slider('enable');
+		} else {
+			slider.slider('disable');
+		}
+	}).change();
 });
 
 // external events listeners
@@ -130,4 +171,45 @@ function handleLimitTimeOptionClick(val) {
 function handleHourConstraintsOptionClick(val) {
 	$('#hour-constraints-overall-header, #hour-constraints-categories-header').hide();
 	$('#hour-constraints-' + val + '-header').show();
+}
+
+/**
+ * Adds a 0 at beggining of string if length 1 (for time display)
+ *
+ * @param {string} to treat
+ */
+function addZeroFirst(string) {
+	if(('' + string).length === 1) {
+		return '0' + string;
+	} else {
+		return string;
+	}
+}
+
+/**
+ * This function updates the position and the title of the tooltip when the slider changes
+ */
+function updateTooltip(event, ui) {
+	var handle = $(ui.handle);
+	var hours = addZeroFirst(parseInt(ui.value/60));
+	var minutes = addZeroFirst(ui.value%60);
+	var timeString = hours + ':' + minutes;
+	if(!handle.next().hasClass('tooltip')) {
+		handle.tooltip({
+			animation: false,
+			placement: 'top',
+			trigger: 'manual',
+			title: timeString,
+		}).tooltip('show');
+	} else {
+		handle.attr('title', timeString).tooltip('fixTitle').tooltip('show');
+	}
+	
+	var tooltip = handle.next();
+	var slider = handle.parent()
+	var width =slider.width();
+	var min = slider.slider('option', 'min');
+	var max = slider.slider('option', 'max');
+	var position = (ui.value-min)*width/(max-min) - tooltip.width()/2;
+	tooltip.css('left', position);
 }
