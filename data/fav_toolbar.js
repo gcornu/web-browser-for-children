6dac59143ba14d0379b74262784bac2d565d379a
florian.clerc@ecl2014.ec-lyon.fr <florian.clerc@ecl2014.ec-lyon.fr>
2014-03-22 10:41:17 +0100

    Added favorite toolbar button image

--- data/fav_toolbar.js
+++ data/fav_toolbar.js
@@ -1,5 +1,9 @@
-self.port.on('favorites_toolbar' , function(favorites){
+self.port.on('favorites_toolbar' , function(data_received){
+	
+	var favorites=data_received.favorites
+	
 	var fav_toolbar=$('<div>');
+	$(fav_toolbar).addClass('child_fav_toolbar');
 	
 	$(favorites).each(function(index, favorite){
 		var link=$('<a>').attr('href' , favorite.uri);
@@ -8,11 +12,9 @@ self.port.on('favorites_toolbar' , function(favorites){
 		$(link).append(image);
 		$(fav_toolbar).append(link);
 	});
-	$(fav_toolbar).addClass('fav_toolbar');
-	
 	
 	var add_favorite_image=$('<img>');
-	$(add_favorite_image).attr('src' , 'star-icon.png');
+	$(add_favorite_image).attr('src' , data_received.star);
 	$(fav_toolbar).append(add_favorite_image);
 
 	$('body').prepend(fav_toolbar);
