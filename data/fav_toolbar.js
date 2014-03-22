e8389e56c1aa4cf76d7e62cdeedc281bee3b7610
florian.clerc@ecl2014.ec-lyon.fr <florian.clerc@ecl2014.ec-lyon.fr>
2014-03-22 09:46:51 +0100

    Reorganizing files for fav_toolbar style

--- data/fav_toolbar.js
+++ data/fav_toolbar.js
@@ -1,19 +1,23 @@
-//window.alert("Page matches ruleset");
-$('body').prepend('hello');
-self.port.emit('require_favorites_toolbar');
-
-self.port.on('favorites_toolbar' , function(){
-console.log('hello2');
-	//window.alert("Page matches ruleset");
-	
-	
-	var style = "padding : 0px; margin : 0px; margin-top:2px; background-color:blue; width:100%; height:100px;";
-
+self.port.on('favorites_toolbar' , function(favorites){
 	var fav_toolbar=$('<div>');
+	
+	$(favorites).each(function(index, favorite){
+		var link=$('<a>').attr('href' , favorite.uri);
+		var image = $('<img>');
+		$(image).attr('src' , 'http://g.etfv.co/'+favorite.uri)
+		$(link).append(image);
+		$(fav_toolbar).append(link);
+	});
 	$(fav_toolbar).addClass('fav_toolbar');
-	$(fav_toolbar).attr('style', style);
-	$(fav_toolbar).prepend('Hello');
-
+	
+	
+	var add_favorite_image=$('<img>');
+	$(add_favorite_image).attr('src' , 'star-icon.png');
+	$(fav_toolbar).append(add_favorite_image);
 
 	$('body').prepend(fav_toolbar);
 });
+
+
+
+//http://getfavicon.appspot.com/
